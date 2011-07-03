5c8f56ec03b0142053ee69999861a8a8606f6a5b
Andreas Bolka <a@bolka.at>
2011-07-03 03:09:19 +0200

    ARM: wrap emit for more natural emission of insns

--- red-system/targets/ARM.r
+++ red-system/targets/ARM.r
@@ -15,13 +15,25 @@ make target-class [
 	stack-width:		4
 	branch-offset-size:	4				;-- @@ ?
 
+	instruction-buffer: make binary! 4
+
+	ei32: func [bin [binary! char! block!]] [
+		;; To allow more natural emission of 32-bit instructions, "ei32"
+		;; collects data in big-endian and emits it as 32-bit chunks in the
+		;; target's native endianness.
+		append instruction-buffer bin
+		if (length? instruction-buffer) >= 4 [
+			emit to-bin32 to-integer take/part instruction-buffer 4
+		]
+	]
+
 	emit-push-integer: func [value [integer!]] [
 		;; @@ we currently store full 32-bit integer immediates directly in the
 		;; instruction stream. should probably use a literal pool instead.
-		emit #{00009fe4}				;-- ldr r0, [pc], #0
-		emit #{0000a0e1}				;-- nop
-		emit to-bin32 value
-		emit #{01002de9}				;-- push {r0}
+		ei32 #{e49f0000}				;-- ldr r0, [pc], #0
+		ei32 #{e1a00000}				;-- nop
+		emit to-bin32 value				;-- <value>
+		ei32 #{e92d0001}				;-- push {r0}
 	]
 
 	emit-push: func [
@@ -39,11 +51,11 @@ make target-class [
 	]
 
 	emit-call-syscall: func [number nargs] [
-		emit shift #{FF} 8 - nargs
-		emit #{00bde8}					;-- pop {r0, .., r<nargs>}
-		emit to-bin8 number
-		emit #{70a0e3}					;-- mov r7, <number>
-		emit #{000000ef}				;-- svc 0			; @@ EABI syscall
+		ei32 #{e8bd00}					;-- pop {r0, .., r<nargs>}
+		ei32 shift #{ff} 8 - nargs
+		ei32 #{e3a070}					;-- mov r7, <number>
+		ei32 to-bin8 number
+		ei32 #{ef000000}				;-- svc 0			; @@ EABI syscall
 	]
 
 	emit-call: func [name [word!] args [block!] sub? [logic!] /local spec fspec] [
