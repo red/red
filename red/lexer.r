e5e9dc1c928a5f0c98a41f19e78f90af678a0064
Nenad Rakocevic <nr@softinnov.com>
2011-10-12 23:42:38 +0200

    FEAT: added file, integer and string rules to lexer.r FEAT: added proper error reporting to the lexer. FIX: several parsing wrong rules.

--- red/lexer.r
+++ red/lexer.r
@@ -15,12 +15,20 @@ lexer: context [
 	count?: yes										;-- if TRUE, lines counter is enabled
 	pos:	none									;-- source input position (error reporting)
 	start:	none
-	end:	none
+	_end:	none
 	value:	none
 	blk: 	none
 	s: e:	none
 	fail:	none
 	
+	throw-error: func [msg [block! string!]][
+		print rejoin [
+			"*** Syntax Error: " uppercase/part reform msg 1
+			"^/*** line: " line
+			"^/*** at: " mold copy/part pos 40
+		]
+		halt
+	]
 	
 	push: func [value][insert/only tail last stack :value]
 
@@ -29,6 +37,18 @@ lexer: context [
 		clear lines
 	]
 	
+	load-integer: func [s [string!]][
+		unless attempt [s: to integer! s][
+			throw-error "invalid 32-bit integer"
+		]
+		s
+	]
+	
+	;--- Parsing rules ---
+	
+	digit: charset "0123465798"
+	
+	;-- UTF-8 encoding rules from: http://tools.ietf.org/html/rfc3629#section-4
 	UTF-8-BOM: #{EFBBBF}
 	ws-ASCII: charset " ^-^M"						;-- ASCII common whitespaces
 	ws-U+2k: charset [#"^(80)" - #"^(8A)"]			;-- Unicode spaces in the U+2000-U+200A range
@@ -58,18 +78,27 @@ lexer: context [
 	UTF8-char: [pos: UTF8-1 | UTF8-2 | UTF8-3 | UTF8-4]
 	
 	not-word-char: charset {/\^,'[](){}"#%$@:;}
+	not-file-char: charset {[](){}"%@:;}
+	not-str-char:  charset {"}
+	not-mstr-char: charset "}"
+	stop: none
 	
-	UTF8-word-char: [
+	UTF8-ws-filtered-char: [
 		[
-			pos: [not-word-char | (count?: no) ws (count?: yes) ] :pos (fail: [end skip])
-			| UTF8-char end: (fail: none)
+			pos: [stop | (count?: no) ws (count?: yes)] :pos (fail: [end skip])
+			| UTF8-char _end: (fail: none)
 		]
+		fail
+	]
+	
+	UTF8-filtered-char: [
+		[pos: stop :pos (fail: [end skip]) | UTF8-char _end: (fail: none)]
 		fail
 	]
 	
 	;-- Whitespaces list from: http://en.wikipedia.org/wiki/Whitespace_character
 	ws: [
-		#"^/" (
+		pos: #"^/" (
 			if count? [
 				line: line + 1 
 				append/only lines tail last stack
@@ -84,20 +113,25 @@ lexer: context [
 			#{9A80}									;-- U+1680 (Ogham space mark)
 			| #{A08E}								;-- U+180E (Mongolian vowel separator)
 		]
-		| #{E280} [
-			ws-U+2k									;-- U+2000-U+200A range
-			| #{A8}									;-- U+2028 (Line separator)
-			| #{A9}									;-- U+2029 (Paragraph separator)
-			| #{AF}									;-- U+202F (Narrow no-break space)
+		| #{E2} [
+			#{80} [
+				ws-U+2k								;-- U+2000-U+200A range
+				| #{A8}								;-- U+2028 (Line separator)
+				| #{A9}								;-- U+2029 (Paragraph separator)
+				| #{AF}								;-- U+202F (Narrow no-break space)
+			]
+			| #{819F}								;-- U+205F (Medium mathematical space)
 		]
-		| #{E2819F}									;-- U+205F (Medium mathematical space)
 		| #{E38080}									;-- U+3000 (Ideographic space)
 	]
 	
 	any-ws: [pos: any ws]
 	
 	
-	word-rule: [start: some UTF8-word-char end:]
+	word-rule: [
+		(stop: not-word-char)
+		start: some UTF8-ws-filtered-char _end:
+	]
 	
 	get-word-rule: [#":" word-rule]
 		
@@ -105,68 +139,95 @@ lexer: context [
 	
 	refinement-rule: [#"/" word-rule]
 	
-	integer-rule: []
+	integer-rule: [some [digit | #"'"] _end:]
+		
+	block-rule: [
+		#"[" (append/only stack make block! 1)
+		any-expression
+		#"]" (		
+			value: last stack
+			remove back tail stack
+			push value
+		)
+	]
 	
+	paren-rule: [
+		#"(" (append/only stack make paren! 1)
+		any-expression
+		#")" (		
+			value: last stack
+			remove back tail stack
+			push value
+		)]
+		
 	char-rule: []
 	
-	block-rule: []
+	encoded-char: [
 	
-	paren-rule: []
+	]
 	
-	string-rule: []
+	string-rule: [
+		  {"} start: (stop: not-str-char)
+		  	;any [UTF8-filtered-char | encoded-char]
+		  	any UTF8-filtered-char
+		  _end: {"}
+		| "{" start: (stop: not-mstr-char)		;@@ need to count LFs
+			;any [UTF8-filtered-char | encoded-char]
+			any UTF8-filtered-char
+		_end: "}"
+	]
 	
 	binary-rule: []
 	
-	file-rule: []
+	file-rule: [
+		#"%" (stop: not-file-char)
+		start: some UTF8-ws-filtered-char _end:
+	]
 	
 	lit-value-rule: []
 	
-	block-rule: [
-		#"[" (append/only stack make block! 1)
-		any expression 
-		#"]" (		
-			blk: last stack
-			remove back tail stack
-			push blk
-		)
+	line-comment-rule: [
+	
+	]
+	
+	multiline-comment-rule: [
+		
 	]
 
 	expression: [
-		any-ws 
-		pos: (end: none) start: [
+		pos: (_end: none) start: [
 			word-rule [
-				#":" 		  (push to set-word!   copy/part start end)
-				| none 		  (push to word! 	   copy/part start end)
+				#":" 		  (push to set-word!   copy/part start _end)
+				| none 		  (push to word! 	   copy/part start _end)
 			]
-			| get-word-rule	  (push to get-word!   copy/part start end)
-			| lit-word-rule	  (push to lit-word!   copy/part start end)
-			| refinement-rule (push to refinement! copy/part start end)
-			;| integer-rule
+			| get-word-rule	  (push to get-word!   copy/part start _end)
+			| lit-word-rule	  (push to lit-word!   copy/part start _end)
+			| refinement-rule (push to refinement! copy/part start _end)
+			| integer-rule	  (push load-integer   copy/part start _end)
 			;| char-rule
 			| block-rule
-			;| paren-rule
-			;| string-rule
+			| paren-rule
+			| string-rule	  (push as-string      copy/part start _end)
 			;| binary-rule
-			;| file-rule
+			| file-rule		  (push to file!	   copy/part start _end)
 			;| lit-value-rule
 		]
-		any-ws
 	]
+	
+	any-expression: [pos: any [expression | ws]]
 
 	header: [any-ws pos: "Red" any-ws block-rule]
 
-	program: [pos: opt UTF-8-BOM header any expression]
+	program: [pos: opt UTF-8-BOM header any-expression]
 	
 	run: func [src [string! binary!]][
 		append/only stack make block! 1
-		
-		unless parse/all src program [		
-			print rejoin [
-				"*** Loading Error: (line " line ") at: " copy/part pos 40
-			]
+
+		unless parse/all/case src program [
+			throw-error "invalid Red data"
 		]
 		
-		add-line-markers blk
+		add-line-markers stack/1
 		also stack/1 clear stack
 	]
 ]
