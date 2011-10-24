65db83c84cc254b89fc2d8e5f711172d7a804307
Nenad Rakocevic <nr@softinnov.com>
2011-10-24 22:40:02 +0200

    FIX: renamed ei32 to emit-32 (a bit less cryptic). FIX: comments realigned, ARM mnemonics capitalized for easier reading. FIX: added 'emit-argument function to make it work with more recent compiler version. FIX: prolog and epilog code commented. The Red/System cconv is stdcall, the ARM emitter needs to conform to it in order to be able to build common debugging tools with other target emitters. The EABI/C convention will be used for callbacks and for calls on imported functions, so the code is just disabled for now.

--- red-system/targets/ARM.r
+++ red-system/targets/ARM.r
@@ -1,12 +1,12 @@
 REBOL [
 	Title:		"Red/System ARM code emitter"
-	Author:		"Andreas Bolka"
+	Author:		"Andreas Bolka, Nenad Rakocevic"
 	File:		%ARM.r
-	Rights:		"Copyright (C) 2011 Andreas Bolka. All rights reserved."
+	Rights:		"Copyright (C) 2011 Andreas Bolka, Nenad Rakocevic. All rights reserved."
 	License:	"BSD-3 - https://github.com/dockimbel/Red/blob/master/BSD-3-License.txt"
 ]
 
-;; AAPCS:
+;; AAPCS (for external calls & callbacks only)
 ;;
 ;;	15 = pc
 ;;	14 = lr
@@ -30,26 +30,26 @@ make target-class [
 	ptr-size:			4
 	default-align:		4
 	stack-width:		4
-	branch-offset-size:	4				;-- @@ ?
+	branch-offset-size:	4							;-- size of branch instruction
 
 	instruction-buffer: make binary! 4
 
-	ei32: func [bin [binary! char! block!]] [
-		;; To allow more natural emission of 32-bit instructions, "ei32"
+	emit-i32: func [bin [binary! char! block!]] [
+		;; To allow more natural emission of 32-bit instructions, "emit-i32"
 		;; collects data in big-endian and emits it as 32-bit chunks in the
 		;; target's native endianness.
-		append instruction-buffer bin
-		if (length? instruction-buffer) >= 4 [
-			emit to-bin32 to-integer take/part instruction-buffer 4
+		insert tail instruction-buffer bin
+		if 4 <= length? instruction-buffer [
+			emit to-bin32 to integer! take/part instruction-buffer 4
 		]
 	]
 
 	emit-load-integer: func [value [integer!]] [
 		;; @@ we currently store full 32-bit integer immediates directly in the
 		;; instruction stream. should probably use a literal pool instead.
-		ei32 #{e49f0000}				;-- ldr r0, [pc], #0
-		ei32 #{e1a00000}				;-- nop
-		emit to-bin32 value				;-- <value>
+		emit-i32 #{e49f0000}						;-- LDR r0, [pc], #0
+		emit-i32 #{e1a00000}						;-- NOP
+		emit to-bin32 value							;-- <value>
 	]
 
 	emit-load: func [
@@ -74,7 +74,7 @@ make target-class [
 		switch/default type?/word value [
 			integer! [
 				emit-load-integer value
-				ei32 #{e92d0001}				;-- push {r0}
+				emit-i32 #{e92d0001}				;-- PUSH {r0}
 			]
 		] [
 			compiler/throw-error join "[codegen] nyi push: " type?/word value
@@ -82,16 +82,16 @@ make target-class [
 	]
 
 	emit-call-syscall: func [number nargs] [
-		ei32 #{e8bd00}					;-- pop {r0, .., r<nargs>}
-		ei32 shift #{ff} 8 - nargs
-		ei32 #{e3a070}					;-- mov r7, <number>
-		ei32 to-bin8 number
-		ei32 #{ef000000}				;-- svc 0			; @@ EABI syscall
+		emit-i32 #{e8bd00}							;-- POP {r0, .., r<nargs>}
+		emit-i32 shift #{ff} 8 - nargs
+		emit-i32 #{e3a070}							;-- MOV r7, <number>
+		emit-i32 to-bin8 number
+		emit-i32 #{ef000000}						;-- SVC 0		; @@ EABI syscall
 	]
 
 	emit-call-native: func [spec] [
 		add-native-reloc spec :reloc-bl
-		ei32 #{eb000000}				;-- bl <disp>
+		emit-i32 #{eb000000}						;-- BL <disp>
 	]
 
 	add-native-reloc: func [spec callback] [
@@ -103,6 +103,21 @@ make target-class [
 		change
 			at code-buf rel-ptr
 			copy/part to-bin32 shift (dst-ptr - rel-ptr - (2 * ptr-size)) 2 3
+	]
+	
+	emit-argument: func [arg func-type [word!]][
+		either all [
+			object? arg
+			any [arg/type = 'logic! 'byte! = first compiler/get-type arg/data]
+			not path? arg/data
+		][
+			unless block? arg [emit-load arg]		;-- block! means last value is already in eax (func call)
+			emit-casting arg no
+			emit-push <last>
+			compiler/last-type: arg/type			;-- for inline unary functions
+		][
+			emit-push either block? arg [<last>][arg]
+		]
 	]
 
 	emit-call: func [name [word!] args [block!] sub? [logic!] /local spec fspec] [
@@ -141,26 +156,26 @@ make target-class [
 		;; that's where the prolog ends. locals, if any, will be pushed on the
 		;; stack immediately afterwards. all other reds-generated code is
 		;; required to be stack neutral.
-		repeat i args-size [
-			ei32 #{e92d00}							;-- push {r<n>}
-			ei32 shift/left #{01} (args-size - i)
-		]
-		unless zero? args-size [
-			ei32 #{e1a0c00d}						;-- mov ip, sp
-		]
-		ei32 #{e92d4ff0}							;-- stmfd sp!, {r4-r11, lr}
-		unless zero? args-size [
-			ei32 #{e1a0b00c}						;-- mov fp, ip
-		]
+	;	repeat i args-size [
+	;		emit-i32 #{e92d00}							;-- push {r<n>}
+	;		emit-i32 shift/left #{01} (args-size - i)
+	;	]
+	;	unless zero? args-size [
+	;		emit-i32 #{e1a0c00d}						;-- mov ip, sp
+	;	]
+	;	emit-i32 #{e92d4ff0}							;-- stmfd sp!, {r4-r11, lr}
+	;	unless zero? args-size [
+	;		emit-i32 #{e1a0b00c}						;-- mov fp, ip
+	;	]
 	]
 
 	emit-epilog: func [name locals [block!] locals-size [integer!]][
 		if verbose >= 3 [print [">>>building:" uppercase mold to-word name "epilog"]]
 
-		unless zero? locals-size [
-			;; Restore sp to where we saved our 9 callee-saved registers.
-			ei32 #{e28bd024}						;-- add sp, fp, #36
-		]
-		ei32 #{e8bd8ff0}							;-- ldmfd sp!, {r4-r11, pc}
+	;	unless zero? locals-size [
+	;		;; Restore sp to where we saved our 9 callee-saved registers.
+	;		emit-i32 #{e28bd024}						;-- add sp, fp, #36
+	;	]
+	;	emit-i32 #{e8bd8ff0}							;-- ldmfd sp!, {r4-r11, pc}
 	]
 ]
