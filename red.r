4961304a395082a43a927b9c5d4138fc3eeb599b
Andreas Bolka <a@bolka.at>
2012-09-16 19:42:14 +0200

    FIX: compile into cwd
    Proposed fix for issue #247

--- red.r
+++ red.r
@@ -96,7 +96,7 @@ redc: context [
 		]
 		opts: make opts config
 		opts/config-name: config-name
-		opts/build-prefix: system/script/path
+		opts/build-prefix: system/options/path
 
 		;; Process -o/--output (if any).
 		if output [
