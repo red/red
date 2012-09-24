cab0d387350b8219a6910d74de241fa75b4bee0a
PeterWAWood <PeterWAWood@gmail.com>
2012-09-24 12:36:32 +0800

    TESTS: Trial Unicode test

---
+++ red/tests/source/runtime/unicode-test.r
@@ -0,0 +1,27 @@
+REBOL [
+  Title:   "Red Unicode test script"
+	Author:  "Peter W A Wood"
+	File: 	 %unicode-test.r
+	Rights:  "Copyright (C) 2011 Nenad Rakocevic & Peter W A Wood. All rights reserved."
+	License: "BSD-3 - https://github.com/dockimbel/Red/blob/origin/BSD-3-License.txt"
+]
+
+#include %../../../runtime/unicode.reds
+
+~~~start-file~~~ "Unicode"
+
+===start-group=== "load utf8"
+
+  --test-- "lutf8-1"
+    --compile-and-run-this {
+      lutf8-1-s: "abcd"
+      lutf8-1-node: unicode/load-utf8 lutf8-1-s 4
+      print "abcd -> "
+      print-hex lutf8-1-node/value
+      print lf
+    }
+  --assert-printed? "abcd -> 61626364h"
+  
+===end-group===
+
+~~~end-file~~~ 
