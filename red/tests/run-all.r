7e90bb2bce8eb885cd97b749999dbd25805f11b5
PeterWAWood <PeterWAWood@gmail.com>
2012-10-01 21:11:16 +0800

    TESTS: Added basic test of load-utf8 in unicode-test.reds

--- red/tests/run-all.r
+++ red/tests/run-all.r
@@ -27,6 +27,7 @@ start-time: now/precise
 
 ===start-group=== "Red/System runtime tests"
   --run-test-file-quiet %source/runtime/tools-test.reds
+  --run-test-file-quiet %source/runtime/unicode-test.reds
 ===end-group===
 
 ===start-group=== "Red Compiler tests"
