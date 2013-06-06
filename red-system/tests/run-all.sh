0c158cd159cf4c27253f7057d4dc4470101a1ca6
PeterWAWood <PeterWAWood@gmail.com>
2013-06-06 23:29:40 +0800

    TESTS: temporary fix to ARM run-all.sh

--- red-system/tests/run-all.sh
+++ red-system/tests/run-all.sh
@@ -1,22 +1,21 @@
 #!/bin/sh
 echo "\nstarting Red/System tests\n"
 echo "Red/System test log\n" > quick-test.log
+failures=0;
 for exe in *;
- 	do
- 		filename=$(basename "$exe")
-		extension="${filename##*.}"
-		filename="${filename%.*}"
- 		if [ "$extension" == "$filename" ]
- 		then
- 			chmod +x $exe;
- 			printf "$exe is running \r";
- 			report=`./$exe`;
- 			echo "$report" >> quick-test.log
- 			case "$report" in 
- 	 	 		*'Number of Assertions Failed:    0'* ) echo "$exe passed             ";;
- 	 	 		* ) echo "****** $exe failed *****";;
- 	 	 	esac
- 	 	fi   
- 	done
+ do
+  if [ "$exe" != "run-all.sh" ] && [ "$exe" != "quick-test.log" ] && [ "$exe" != "libtest-dll1.so" ] && [ "$exe" != "libtest-dll2.so" ]
+   then
+     chmod +x $exe;
+     printf "$exe is running \r";
+     report=`./$exe`;
+     failures=0;
+     echo "$report" >> quick-test.log
+     case "$report" in 
+     *'Number of Assertions Failed:    0'* ) echo "$exe passed             ";;
+     * ) echo "****** $exe failed *****";;
+     esac
+  fi   
+ done
 echo "\nfinished Red/System tests\n"
 echo ""
