142112489a98f2ddffffaa714f10e42297295577
PeterWAWood <PeterWAWood@gmail.com>
2013-08-03 09:44:24 +0800

    TESTS: added mold tests to interpreter tests

--- red/tests/source/units/make-interpreter-auto-test.r
+++ red/tests/source/units/make-interpreter-auto-test.r
@@ -32,6 +32,7 @@ file-list: [
 	%insert-test.red									%auto-tests/interpreter-insert-test.red
 	%make-test.red									    %auto-tests/interpreter-make-test.red
 	%system-test.red									%auto-tests/interpreter-system-test.red
+	%mold-test.red										%auto-tests/interpreter-mold-test.red
 	%auto-tests/equal-auto-test.red						%auto-tests/interp-equal-auto-test.red
 	%auto-tests/greater-auto-test.red					%auto-tests/interp-greater-auto-test.red
 	%auto-tests/infix-equal-auto-test.red				%auto-tests/interp-inf-equal-auto-test.red
