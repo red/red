0a856a9906a5afa500fc2bfd95f3fb7ec970dc80
Nenad Rakocevic <nr@red-lang.org>
2013-08-03 19:24:51 +0200

    FIX: issue #493 (Error with 'print-line and integer!s)

--- red-system/compiler.r
+++ red-system/compiler.r
@@ -2595,6 +2595,10 @@ system-dialect: make-profilable context [
 			either block? expr [
 				type: comp-call expr/1 next expr 		;-- function call case (recursive)
 				if type [last-type: type]				;-- set last-type if not already set
+				if all [variable boxed][				;-- process casting if result assigned to variable
+					emitter/target/emit-casting boxed no	;-- insert runtime type casting if required
+					last-type: boxed/type
+				]
 			][
 				last-type: either not any [
 					all [new? literal? unbox expr]		;-- if new variable, value will be store in data segment
