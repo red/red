63229765d972a62be22aa36a79d9dc9be20f8569
Nenad Rakocevic <nr@red-lang.org>
2013-08-06 17:26:33 +0200

    FIX: wrong flag value for BSS section (issue #479).

--- red-system/formats/PE.r
+++ red-system/formats/PE.r
@@ -132,7 +132,7 @@ context [
 			write				#{80000000}	;-- can be written to
 		]
 		s-type [
-			BSS					#{C0000000}	;-- [read write]
+			BSS					#{C0000080}	;-- [read write uninitialized]
 			data				#{C0000040}	;-- [read write initialized]
 			export				#{40000040}	;-- [read initialized]
 			import				#{C0000040}	;-- [read write initialized]
