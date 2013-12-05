06a7bfaaa10c31269d5b2f65862b4975315c0e26
Nenad Rakocevic <nr@red-lang.org>
2013-12-05 14:53:54 +0100

    FIX: issue #589 (Crashing with semicolon placement)

--- lexer.r
+++ lexer.r
@@ -60,7 +60,7 @@ lexer: context [
 	not-mstr-char:  #"}"
 	caret-char:	    charset [#"^(40)" - #"^(5F)"]
 	non-printable-char: charset [#"^(00)" - #"^(1F)"]
-	integer-end:	charset {^{"])}
+	integer-end:	charset {^{"]);}
 	stop: 		    none
 	
 	control-char: reduce [
