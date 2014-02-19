4ee69bd76eb79a1c8857bd9da91836402864bc6b
PeterWAWood <PeterWAWood@gmail.com>
2014-02-20 07:09:20 +0800

    TESTS: updated and reformatted print-test.r

--- tests/source/compiler/print-test.r
+++ tests/source/compiler/print-test.r
@@ -9,41 +9,43 @@ REBOL [
 
 ~~~start-file~~~ "Red print"
 
- --test-- "Red print 1"
-   --compile-and-run-this-red {print 1} 
-  --assert-red-printed? 1
+ 	--test-- "Red print 1"
+ 		--compile-and-run-this {Red[] print 1} 
+ 		--assert-printed? 1
   
-  --test-- "Red print 2"
-  --compile-and-run-this-red {print [1 2 3]}
-  --assert-red-printed? 1 2 3
+ 	--test-- "Red print 2"
+  		--compile-and-run-this {Red[] print [1 2 3]}
+  		--assert-printed? 1 2 3
   
-  --test-- "Red print 3"
-    --compile-and-run-this-red {
-      s: "12345"
-      forall s [prin s]
-    }
-  --assert-red-printed? "123452345345455"
+  	--test-- "Red print 3"
+    	--compile-and-run-this {
+    		Red[] 
+    		s: "12345"
+    		forall s [prin s]
+    	}
+    	--assert-printed? "123452345345455"
   
-  --test-- "Red print 4"
-    --compile-and-run-this-red {
-      s: "12345"
-      prin "***"
-      prin next s
-      print "***"
-    }
-  --assert-red-printed? "***2345***"
+    --test-- "Red print 4"
+    	--compile-and-run-this {
+    		Red[] 
+    		s: "12345"
+    		prin "***"
+    		prin next s
+    		print "***"
+    	}
+    	--assert-printed? "***2345***"
   
-  --test-- "issue #427"
-    --compile-and-run-this-red {
-      issue427-f: func [
-        /local count
-      ][
-        repeat count 5 [
-          prin count
-        ]
-      ]
-      issue427-f
-    }
-  --assert-red-printed? "12345"
+    --test-- "issue #427"
+    	--compile-and-run-this {
+    		issue427-f: func [
+    			/local count
+    		][
+    			repeat count 5 [
+    			prin count
+    			]
+    		]
+    		issue427-f
+    	}
+    	--assert-printed? "12345"
   
 ~~~end-file~~~ 
