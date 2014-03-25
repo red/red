1926dead5720ea96afbfcf7953166d5e91d27b82
PeterWAWood <PeterWAWood@gmail.com>
2014-03-25 12:47:14 +0800

    TESTS: updated Red 'call tests

--- tests/source/library/call-test.r
+++ tests/source/library/call-test.r
@@ -1,6 +1,6 @@
 REBOL [
 	Title:   "Red call tests"
-	Author:  "Peter W A Wood"
+	Author:  "Bruno Anselme & Peter W A Wood"
 	File: 	 %call-test.r
 	Tabs:	 4
 	Rights:  "Copyright (C) 2014 Bruno Anselme & Peter W A Wood. All rights reserved."
@@ -13,22 +13,61 @@ REBOL [
 		if exists? %call-test [delete %call-test]
 		if exists? %call-test.exe [delete %call-test.exe]
 		path-to-rebol: to-local-file system/options/boot
+		if qt/windows-os? [
+			path-to-rebol: join {^"} [path-to-rebol {^"}]	
+		]
 		path-to-red: to-local-file clean-path %../../red.r
 		path-to-source: to-local-file clean-path %../../tests/source/library/call-test.red
 		src: join {Red[] } compose [
-			{ #include %../../system/library/call/call.red
-			call/wait "} (path-to-rebol)
-			" -qs " (path-to-red) { }
-			(path-to-source) {"}
+			" #include %../../system/library/call/call.red "
+			"call/wait {" (path-to-rebol)
+			" -qs " (path-to-red) " "
+			" -o " (qt/runnable-dir) " "
+			(path-to-source) "}"
 		] 
 		--compile-and-run-this src
-	    --assert exists? any [%call-test %call-test.exe]
-   
+		--assert any [
+	    	exists? %call-test
+	    	exists? %call-test.exe
+	    ]
+	    
 	--test-- "call-2"
+		if exists? %call-test [delete %call-test]
+		if exists? %call-test.exe [delete %call-test.exe]
+		path-to-rebol: to-local-file system/options/boot
+		if qt/windows-os? [
+			path-to-rebol: join {^"} [path-to-rebol {^"}]	
+		]
+		path-to-red: to-local-file clean-path %../../red.r
+		path-to-source: to-local-file clean-path %../../tests/source/library/call-test.red
+		src: join {Red[] } compose [
+			" #include %../../system/library/call/call.red "
+			"call {" (path-to-rebol)
+			" -qs " (path-to-red) " "
+			" -o " (qt/runnable-dir) " "
+			(path-to-source) "}"
+		] 
+		--compile-and-run-this src
+		--assert all [
+	    	not exists? %call-test
+	    	not exists? %call-test.exe
+	    ]
+	    
+	--test-- "call-3"
 		--compile %tests/source/library/call-test.red
-		output: ""
+		output: copy ""
 	    exe: either qt/windows-os? ["call-test.exe"] ["./call-test"]
+	    exe: join exe " call-1"
 	    call/output exe output
 	    --assert "1^/" = output
+	    
+	--test-- "call-4"
+		error: copy ""
+		output: copy ""
+	    exe: either qt/windows-os? ["not-call-test.exe"] ["./not-call-test"]
+	    exe: join exe
+	    call/output/error exe output error
+	    --assert "" <> error 
+	    --assert "" = output
 	    
 ~~~end-file~~~
--- tests/source/library/call-test.red
+++ tests/source/library/call-test.red
@@ -1,10 +1,34 @@
 Red [
 	Title:   "Red call test program"
-	Author:  "Peter W A Wood"
+	Author:  "Bruno Anselme & Peter W A Wood"
 	File: 	 %call-test.red
 	Tabs:	 4
 	Rights:  "Copyright (C) 2013 Bruno Anselme & Peter W A Wood. All rights reserved."
 	License: "BSD-3 - https://github.com/dockimbel/Red/blob/origin/BSD-3-License.txt"
 ]
+read-argument: routine [
+	/local
+		args [str-array!]
+		str	 [red-string!]
+][
+	if system/args-count <> 2 [
+		SET_RETURN(none-value)
+		exit
+	]
+	args: system/args-list + 1							;-- skip binary filename
+	str: string/load args/item (1 + length? args/item) UTF-8
+	SET_RETURN(str)
+]
 
-print 1
+test-name: read-argument
+
+if test-name = "call-1" [				
+	print 1	
+	quit
+]
+
+if test-name = "call-2" [				
+	1 / 0
+	quit
+]
+
