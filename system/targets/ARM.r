76d894f95b96eb365c2106dd79185637fb332bc5
Nenad Rakocevic <nr@red-lang.org>
2014-06-26 15:23:41 +0800

    FIX: issue #861 (Handling float! pointer argument incorrectly in function on Linux-ARM)

--- system/targets/ARM.r
+++ system/targets/ARM.r
@@ -1451,9 +1451,11 @@ make-profilable make target-class [
 			]
 			path! [
 				emitter/access-path value none
-				if cast [
+				compiler/last-type: either cast [
 					emit-casting cast no
-					compiler/last-type: cast/type
+					cast/type
+				][
+					compiler/resolve-path-type value
 				]
 				emit-push <last>
 			]
