517e81e145bfd75846e0306006a98652958232cb
Nenad Rakocevic <nr@red-lang.org>
2015-06-01 14:49:43 +0800

    FIX: additional header minor fixes in test files.

--- tests/source/units/make-not-equal-auto-test.r
+++ tests/source/units/make-not-equal-auto-test.r
@@ -20,7 +20,7 @@ Red [
 	Author:  "Nenad Rakocevic & Peter W A Wood"
 	File: 	 ***FILE***
 	Tabs:	 4
-	Rights:  "Copyright (C) 2011-2012, 2012 Nenad Rakocevic & Peter W A Wood. All rights reserved."
+	Rights:  "Copyright (C) 2011-2015, Nenad Rakocevic & Peter W A Wood. All rights reserved."
 	License: "BSD-3 - https://github.com/dockimbel/Red/blob/origin/BSD-3-License.txt"
 ]
 
