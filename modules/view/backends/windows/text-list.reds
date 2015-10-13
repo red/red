red/red

commit 54d69747bee0ce5c89f605a0b4ad0c89ae537b91

FIX: crash when insert word! into drop-list's data field

modules/view/backends/windows/text-list.reds
@@ -77,6 +77,11 @@ insert-list-item: func [
 		msg	[integer!]
 		len [integer!]
 ][
+	unless TYPE_OF(item) = TYPE_STRING [exit]
+
+	len: as-integer SendMessage hWnd CB_GETCOUNT 0 0
+	if pos > len [pos: len]
+
 	str: unicode/to-utf16 item
 	msg: either drop? [CB_INSERTSTRING][LB_INSERTSTRING]
 	SendMessage hWnd msg pos as-integer str

modules/view/backends/windows/win32.reds
@@ -42,6 +42,7 @@ Red/System [
 
 #define CB_ADDSTRING		0143h
 #define CB_DELETESTRING		0144h
+#define CB_GETCOUNT			0146h
 #define CB_GETCURSEL		0147h
 #define CB_GETLBTEXT		0148h
 #define CB_GETLBTEXTLEN		0149h

commit b3ac360dc75f0d81cc604a174aaafe096b3ade4e

FIX: regression on text-list item insertion.
Regression caused by commit:
https://github.com/red/red/commit/54d69747bee0ce5c89f605a0b4ad0c89ae537b91

modules/view/backends/windows/text-list.reds
@@ -79,7 +79,8 @@ insert-list-item: func [
 ][
 	unless TYPE_OF(item) = TYPE_STRING [exit]
 
-	len: as-integer SendMessage hWnd CB_GETCOUNT 0 0
+	msg: either drop? [CB_GETCOUNT][LB_GETCOUNT]
+	len: as-integer SendMessage hWnd msg 0 0
 	if pos > len [pos: len]
 
 	str: unicode/to-utf16 item

modules/view/backends/windows/win32.reds
@@ -23,6 +23,7 @@ Red/System [
 #define LB_DELETESTRING		0182h
 #define LB_RESETCONTENT		0184h
 #define LB_GETCURSEL		0188h
+#define LB_GETCOUNT			018Bh
 #define LB_SETHORIZONTALEXTENT	0194h
 
 #define HORZRES				8

tests/view-test.red
@@ -6,8 +6,6 @@ Red [
 
 system/view/debug?: yes
 live?: system/view/auto-sync?: no
-probe live?
-probe system/view/auto-sync?
 
 workstation?: system/view/platform/product = 1
 
@@ -226,7 +224,6 @@ button/font: make font! [
 	size: 12
 	style: 'bold
 ]
-probe button/font
 
 win/pane: reduce [
 	make face! [