1b3f3ca33c1175fb4d686592a4894fcafd48908a
Nenad Rakocevic <nr@red-lang.org>
2016-10-07 20:52:13 +0800

    FIX: emit proper variadic layout depending on the calling convention both for natives and imported functions.
    TBD: port those fixes to ARM backend.

--- system/targets/IA-32.r
+++ system/targets/IA-32.r
@@ -1815,7 +1815,20 @@ make-profilable make target-class [
 		]
 	]
 	
-	emit-call-import: func [args [block!] fspec [block!] spec [block!] attribs [block! none!]][
+	emit-variadic-data: func [args [block!] /local total][
+		emit-push call-arguments-size? args/2		;-- push arguments total size in bytes 
+													;-- (required to clear stack on stdcall return)
+		emit #{8D742404}							;-- LEA esi, [esp+4]	; skip last pushed value
+		emit #{56}									;-- PUSH esi			; push arguments list pointer
+		total: length? args/2
+		if args/1 = #typed [total: total / 3]		;-- typed args have 3 components
+		emit-push total								;-- push arguments count
+	]
+	
+	emit-call-import: func [args [block!] fspec [block!] spec [block!] attribs [block! none!] /local cdecl?][
+		cdecl?: fspec/3 = 'cdecl
+		if all [issue? args/1 not cdecl?][emit-variadic-data args]
+
 		either compiler/job/OS = 'MacOSX [
 			either PIC? [
 				emit #{8D83}						;-- LEA eax, [ebx+disp]	; PIC
@@ -1827,12 +1840,16 @@ make-profilable make target-class [
 		][
 			emit-indirect-call spec
 		]
-		if fspec/3 = 'cdecl [						;-- add calling cleanup when required
-			emit-cdecl-pop fspec args
-		]
+		if cdecl? [emit-cdecl-pop fspec args]		;-- add calling cleanup when required
 	]
 
-	emit-call-native: func [args [block!] fspec [block!] spec [block!] attribs [block! none!] /routine name [word!] /local total][
+	emit-call-native: func [
+		args [block!] fspec [block!] spec [block!] attribs [block! none!]
+		/routine name [word!]
+		/local cdecl?
+	][
+		cdecl?: fspec/3 = 'cdecl
+		
 		either routine [
 			either 'local = last fspec [
 				name: pick tail fspec -2
@@ -1846,21 +1863,11 @@ make-profilable make target-class [
 				emit-indirect-call spec
 			]
 		][
-			if issue? args/1 [							;-- variadic call
-				emit-push call-arguments-size? args/2	;-- push arguments total size in bytes 
-														;-- (required to clear stack on stdcall return)
-				emit #{8D742404}						;-- LEA esi, [esp+4]	; skip last pushed value
-				emit #{56}								;-- PUSH esi			; push arguments list pointer
-				total: length? args/2
-				if args/1 = #typed [total: total / 3]	;-- typed args have 3 components
-				emit-push total							;-- push arguments count
-			]
+			if all [issue? args/1 not cdecl?][emit-variadic-data args]
 			emit #{E8}								;-- CALL NEAR disp
 			emit-reloc-addr spec					;-- 32-bit relative displacement
 		]
-		if fspec/3 = 'cdecl [						;-- in case of non-default calling convention
-			emit-cdecl-pop fspec args
-		]
+		if cdecl? [emit-cdecl-pop fspec args]		;-- in case of non-default calling convention
 	]
 	
 	emit-stack-align: does [
