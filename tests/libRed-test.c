654a12aa0f7a4958494204d54f06a5726526a632
Nenad Rakocevic <nr@red-lang.org>
2016-11-20 20:41:09 +0800

    FEAT: extra tests for libRed.

--- tests/libRed-test.c
+++ tests/libRed-test.c
@@ -3,12 +3,14 @@
 int main() {
 	redBoot();
 	int a = redSymbol("a");
+	red_word Print = redWord("print");
 	
 	redSetGlobalWord(a, (red_value) redBlock(redInteger(42), redString("hello"), 0));
 	redDo("?? a foreach v a [probe v]");
 	redPrint(redGetGlobalWord(a));
 
 	redProbe(redCall(redWord("what-dir"), 0));
-	redCall(redWord("print"), redFloat(99.0), 0);
+	redCall(redWord("print"), redDo("system/version"), 0);
+	redCall(Print, redFloat(99.0), 0);
 	return 0;
 }
