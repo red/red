red/red

commit 40c3358ae24faa80caa9a33e01a6795e20784c29

FIX: additional fix for #3173

modules/view/backends/windows/events.reds
@@ -1192,6 +1192,7 @@ process: func [
 		res	   [integer!]
 		x	   [integer!]
 		y	   [integer!]
+		track  [tagTRACKMOUSEEVENT value]
 		evt?   [logic!]
 ][
 	switch msg/msg [
@@ -1223,10 +1224,20 @@ process: func [
 					null? hover-saved
 				]
 			][
+				track/cbSize: size? tagTRACKMOUSEEVENT
+				track/dwFlags: 2						;-- TME_LEAVE
+				track/hwndTrack: new
+				TrackMouseEvent :track
 				msg/hWnd: new
 				make-event msg 0 EVT_OVER
 			]
 			hover-saved: new
+			EVT_DISPATCH
+		]
+		WM_MOUSELEAVE [
+			msg/hWnd: hover-saved
+			make-event msg EVT_FLAG_AWAY EVT_OVER
+			hover-saved: null
 			EVT_DISPATCH
 		]
 		WM_MOUSEWHELL [

modules/view/backends/windows/win32.reds
@@ -304,6 +304,7 @@ Red/System [
 #define WM_EXITSIZEMOVE		0232h
 #define WM_IME_SETCONTEXT	0281h
 #define WM_IME_NOTIFY		0282h
+#define WM_MOUSELEAVE		02A3h
 #define WM_DPICHANGED		02E0h
 #define WM_COPY				0301h
 #define WM_PASTE			0302h
@@ -602,6 +603,13 @@ tagTEXTMETRIC: alias struct! [
 	tmStruckOut			[byte!]
 	tmPitchAndFamily	[byte!]
 	tmCharSet			[byte!]
+]
+
+tagTRACKMOUSEEVENT: alias struct! [
+	cbSize		[integer!]
+	dwFlags		[integer!]
+	hwndTrack	[handle!]
+	dwHoverTime	[integer!]
 ]
 
 tagNMHDR: alias struct! [
@@ -1001,6 +1009,10 @@ XFORM!: alias struct! [
 		]
 	]
 	"User32.dll" stdcall [
+		TrackMouseEvent: "TrackMouseEvent" [
+			EventTrack	[tagTRACKMOUSEEVENT]
+			return:		[logic!]
+		]
 		RedrawWindow: "RedrawWindow" [
 			hWnd		[handle!]
 			lprcUpdate	[RECT_STRUCT]

commit 41538b4d48b0c3f0325a2d6cd8dd99fab186544b

FIX: regression caused by commit [40c3358].

modules/view/backends/windows/events.reds
@@ -1190,6 +1190,7 @@ process: func [
 		pt	   [tagPOINT]
 		hWnd   [handle!]
 		new	   [handle!]
+		saved  [handle!]
 		res	   [integer!]
 		x	   [integer!]
 		y	   [integer!]
@@ -1209,6 +1210,7 @@ process: func [
 			][
 				return EVT_DISPATCH						;-- filter out buggy mouse positions (thanks MS!)
 			]
+			saved: msg/hWnd
 			new: get-child-from-xy msg/hWnd x y
 			
 			evt?: all [hover-saved <> null hover-saved <> new]
@@ -1233,12 +1235,15 @@ process: func [
 				make-event msg 0 EVT_OVER
 			]
 			hover-saved: new
+			msg/hWnd: saved
 			EVT_DISPATCH
 		]
 		WM_MOUSELEAVE [
+			saved: msg/hWnd
 			msg/hWnd: hover-saved
 			make-event msg EVT_FLAG_AWAY EVT_OVER
 			hover-saved: null
+			msg/hWnd: saved
 			EVT_DISPATCH
 		]
 		WM_MOUSEWHELL [