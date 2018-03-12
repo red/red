red/red

commit 9178838f7569b6e3d28fedcf070f04ab7e6e4e33

FEAT: setting words rebound to a reactor's context now triggers object's `on-change*` event.
Fixes issue #3091. Now the following code will trigger the reaction:

a: make reactor! [b: 5 c: is [b]]
do bind [b: 2] a
;; a/c = 2

modules/view/view.red
@@ -234,7 +234,7 @@ on-face-deep-change*: function [owner word target action new index part state fo
 	]
 ]
 
-link-tabs-to-parent: function [face [object!] /init][
+link-tabs-to-parent: function [face [object!]][
 	if faces: face/pane [
 		visible?: face/visible?
 		forall faces [
@@ -242,7 +242,6 @@ link-tabs-to-parent: function [face [object!] /init][
 				faces/1/visible?: make logic! all [visible? face/selected = index? faces]
 			]
 			faces/1/parent: face
-			if init [show/with faces/1 face]
 		]
 	]
 ]
@@ -334,7 +333,6 @@ face!: object [				;-- keep in sync with facet! enum
 						]
 					]
 				]
-				if type = 'tab-panel [link-tabs-to-parent/init self] ;-- panels need to be SHOWn before parent
 			]
 			if all [not same-pane? any [series? old object? old]][modify old 'owned none]
 			

environment/routines.red
@@ -22,11 +22,15 @@ set-quiet: routine [
 	word  [any-type!]
 	value [any-type!]
 	/local
+		w	 [red-word!]
 		type [integer!]
+		node [node!]
 ][
 	type: TYPE_OF(word)
 	unless ANY_WORD?(type) [ERR_EXPECT_ARGUMENT(TYPE_WORD 0)]
-	_context/set as red-word! word stack/arguments + 1
+	w: as red-word! word
+	node: w/ctx
+	_context/set-in w stack/arguments + 1 TO_CTX(node) no
 ]
 
 ;-- Following definitions are used to create op! corresponding operators

runtime/datatypes/context.reds
@@ -226,12 +226,17 @@ _context: context [
 	]
 
 	set-in: func [
-		word 		[red-word!]
-		value		[red-value!]
-		ctx			[red-context!]
-		return:		[red-value!]
+		word 	[red-word!]
+		value	[red-value!]
+		ctx		[red-context!]
+		event?	[logic!]								;-- TRUE: trigger object events
+		return:	[red-value!]
 		/local
 			values	[series!]
+			obj		[red-object!]
+			slot	[red-value!]
+			old		[red-value!]
+			s		[series!]
 	][
 		#if debug? = yes [if verbose > 0 [print-line "_context/set-in"]]
 		
@@ -246,7 +251,22 @@ _context: context [
 			copy-cell value (as red-value! ctx/values) + word/index
 		][
 			values: as series! ctx/values/value
-			copy-cell value values/offset + word/index
+			slot: values/offset + word/index
+			
+			if event? [
+				s: as series! ctx/self/value
+				obj: as red-object! s/offset + 1
+				
+				if all [TYPE_OF(obj) = TYPE_OBJECT obj/on-set <> null][
+					old: stack/push slot
+					word: as red-word! word
+					copy-cell value slot
+					object/fire-on-set obj word old value
+					stack/top: old - 1
+					return slot
+				]
+			]
+			copy-cell value slot
 		]
 	]
 	
@@ -260,7 +280,7 @@ _context: context [
 		#if debug? = yes [if verbose > 0 [print-line "_context/set"]]
 
 		node: word/ctx
-		set-in word value TO_CTX(node)
+		set-in word value TO_CTX(node) yes
 	]
 	
 	get-in: func [

runtime/datatypes/object.reds
@@ -1108,7 +1108,7 @@ object: context [
 		
 		either value <> null [
 			if on-set? [old: stack/push _context/get-in word ctx]
-			_context/set-in word value ctx
+			_context/set-in word value ctx no
 			if on-set? [fire-on-set parent as red-word! element old value]
 			res: value
 		][

runtime/natives.reds
@@ -645,7 +645,7 @@ natives: context [
 				stack/set-last value
 			]
 			default [
-				set-word w value
+				_context/set w value
 				stack/set-last value
 			]
 		]
@@ -2840,32 +2840,6 @@ natives: context [
 		]
 	]
 	
-	set-word: func [
-		w	  [red-word!]
-		value [red-value!]
-		/local
-			ctx	 [red-context!]
-			obj	 [red-object!]
-			slot [red-value!]
-			old	 [red-value!]
-			node [node!]
-			s	 [series!]
-	][
-		node: w/ctx
-		ctx: TO_CTX(node)
-		s: as series! ctx/self/value
-		obj: as red-object! s/offset + 1
-
-		either all [TYPE_OF(obj) = TYPE_OBJECT obj/on-set <> null][
-			slot: _context/get w
-			old: stack/push slot
-			copy-cell value slot
-			object/fire-on-set obj w old value
-		][
-			_context/set w value
-		]
-	]
-	
 	set-many: func [
 		words [red-block!]
 		value [red-value!]
@@ -2898,7 +2872,7 @@ natives: context [
 				][
 					fire [TO_ERROR(script invalid-arg) w]
 				]
-				set-word w v
+				_context/set w v
 			]
 			i: i + 1
 		]

commit 505a3b443cc6edfb7bd9f6bc85a4ec2c283d083e

FIX: regression on dynamic panels setting in tab-panels.
Panels were not initialized anymore when set dynamically. Caused by commit 9178838f7569b6e3d28fedcf070f04ab7e6e4e33

modules/view/view.red
@@ -234,7 +234,7 @@ on-face-deep-change*: function [owner word target action new index part state fo
 	]
 ]
 
-link-tabs-to-parent: function [face [object!]][
+link-tabs-to-parent: function [face [object!] /init][
 	if faces: face/pane [
 		visible?: face/visible?
 		forall faces [
@@ -242,6 +242,7 @@ link-tabs-to-parent: function [face [object!]][
 				faces/1/visible?: make logic! all [visible? face/selected = index? faces]
 			]
 			faces/1/parent: face
+			if init [show/with faces/1 face]
 		]
 	]
 ]
@@ -332,6 +333,9 @@ face!: object [				;-- keep in sync with facet! enum
 							system/view/platform/destroy-view f no
 						]
 					]
+				]
+				if all [not same-pane? type = 'tab-panel self/state][
+					link-tabs-to-parent/init self
 				]
 			]
 			if all [not same-pane? any [series? :old object? :old]][modify old 'owned none]