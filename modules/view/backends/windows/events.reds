red/red

commit 38845fc3acdcfe54642c096ed6a4f0628b2f5740

FIX: issue #3264 (Radio widget turned off does not generate any event)

modules/view/backends/windows/events.reds
@@ -602,17 +602,18 @@ process-command-event: func [
 	child: as handle! lParam
 	unless null? current-msg [saved: current-msg/hWnd]
 	switch WIN32_HIWORD(wParam) [
+		BN_UNPUSHED
 		BN_CLICKED [
 			type: as red-word! get-facet current-msg FACE_OBJ_TYPE
-			make-event current-msg 0 EVT_CLICK			;-- should be *after* get-facet call (Windows closing on click case)
+			if WIN32_HIWORD(wParam) = BN_CLICKED [
+				make-event current-msg 0 EVT_CLICK		;-- should be *after* get-facet call (Windows closing on click case)
+			]
 			if any [
 				type/symbol = check
 				type/symbol = radio
 			][
 				current-msg/hWnd: child					;-- force child handle
-				if get-logic-state current-msg [
-					make-event current-msg 0 EVT_CHANGE
-				]
+				make-event current-msg 0 EVT_CHANGE
 			]
 		]
 		EN_CHANGE [											;-- sent also by CreateWindow

modules/view/backends/windows/gui.reds
@@ -1849,6 +1849,7 @@ change-data: func [
 		]
 		type = radio [
 			set-logic-state hWnd as red-logic! data no
+			SendMessage GetParent hWnd WM_COMMAND BN_UNPUSHED << 16 as-integer hWnd
 		]
 		type = tab-panel [
 			set-tabs hWnd get-face-values hWnd

modules/view/backends/windows/win32.reds
@@ -327,6 +327,7 @@ Red/System [
 #define BM_SETIMAGE			F7h
 
 #define BN_CLICKED 			0
+#define BN_UNPUSHED         3
 
 #define BST_UNCHECKED		0
 #define BST_CHECKED			1

commit 48d2b03f4b6c75e9945f526234b05b86cdb3ecd2

FIX: issue #3279 ('check' value not changed on clicking)
Regression caused by commit: https://github.com/red/red/commit/38845fc3acdcfe54642c096ed6a4f0628b2f5740

modules/view/backends/windows/events.reds
@@ -613,7 +613,12 @@ process-command-event: func [
 				type/symbol = radio
 			][
 				current-msg/hWnd: child					;-- force child handle
-				make-event current-msg 0 EVT_CHANGE
+				if any [
+					type/symbol = radio
+					get-logic-state current-msg
+				][
+					make-event current-msg 0 EVT_CHANGE
+				]
 			]
 		]
 		EN_CHANGE [											;-- sent also by CreateWindow