red/red

commit 921edf740f22abb3e95663b3b95c6b8e00626ca8

FIX: including %input.red causes compiler error on non-Windows platforms.

environment/functions.red
@@ -41,7 +41,7 @@ quit: func [
 	/return status	[integer!] "Return an exit status"
 ][
 	#if config/OS <> 'Windows [
-		if system/console [system/console/terminate]
+		if system/console [_terminate-console]
 	]
 	quit-return any [status 0]
 ]

environment/console/engine.red
@@ -96,13 +96,6 @@ system/console: context [
 		]
 	]
 
-	terminate: routine [][
-		#if OS <> 'Windows [
-		#if gui-console? = no [
-			if terminal/init? [terminal/emit-string "^[[?2004l"]	;-- disable bracketed paste mode
-		]]
-	]
-
 	count-delimiters: function [
 		buffer	[string!]
 		/extern count

environment/console/input.red
@@ -573,6 +573,13 @@ _read-input: routine [prompt [string!]][
 	terminal/edit prompt
 ]
 
+_terminate-console: routine [][
+	#if OS <> 'Windows [
+	#if gui-console? = no [
+		if terminal/init? [terminal/emit-string "^[[?2004l"]	;-- disable bracketed paste mode
+	]]
+]
+
 ask: function [
 	question [string!]
 	return:  [string!]

commit ca2344a73f555ddaf0a4ecf28b61ba09ac3e88b7

FIX: compiling error on non-Windows platforms caused by commit 921edf7.

environment/functions.red
@@ -41,7 +41,7 @@ quit: func [
 	/return status	[integer!] "Return an exit status"
 ][
 	#if config/OS <> 'Windows [
-		if system/console [_terminate-console]
+		if system/console [do [_terminate-console]]
 	]
 	quit-return any [status 0]
 ]