89d4ce92e09323f522b38bb2f0c4d0ae88c895ff
Boleslav Březovský <boleslav.brezovsky@nic.cz>
2019-02-04 14:08:45 +0100

    FIX: #3763 fixed (-0.0 checks for additional digits)

--- lexer.r
+++ lexer.r
@@ -426,7 +426,7 @@ lexer: context [
 	]
 
 	decimal-special: [
-		s: "-0.0" | (neg?: no) opt [#"-" (neg?: yes)] "1.#" s: [
+		s: "-0.0" pos: [integer-end | ws-no-count | end ] :pos | (neg?: no) opt [#"-" (neg?: yes)] "1.#" s: [
 			[[#"N" | #"n"] [#"a" | #"A"] [#"N" | #"n"]]
 			| [[#"I" | #"i"] [#"N" | #"n"] [#"F" | #"f"]]
 		]
