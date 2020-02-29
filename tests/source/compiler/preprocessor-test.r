7b93056aa8d3daa7d4797ab0dc38b3e536225a99
hiiamboris <hiiamboris@gmail.com>
2020-02-29 19:44:38 +0300

    FIX: no images for linux users!

--- tests/source/compiler/preprocessor-test.r
+++ tests/source/compiler/preprocessor-test.r
@@ -116,12 +116,12 @@ REBOL [
 			prin "*test11* " probe preprocessor/fetch-next [a: o b: 1]
 			prin "*test12* " probe preprocessor/fetch-next [f o f 1]
 			prin "*test13* " probe preprocessor/fetch-next [b/o/f o b/o/f/x 1 2 3]
-			i: make image! 10x10 s: "abcd"
-			b: reduce [i s]
-			w1: 'i w2: 's w3: 3
-			prin "*test14* " probe preprocessor/fetch-next [i/size s/3]
+			p: 10x20 s: "abcd"
+			b: reduce [p s]
+			w1: 'p w2: 's w3: 3
+			prin "*test14* " probe preprocessor/fetch-next [p/y s/3]
 			prin "*test15* " probe preprocessor/fetch-next [s/3]
-			prin "*test16* " probe preprocessor/fetch-next [b/:w1/size b/(w2)/:w3]
+			prin "*test16* " probe preprocessor/fetch-next [b/:w1/y b/(w2)/:w3]
 			prin "*test17* " probe preprocessor/fetch-next [b/(w2)/:w3]
 		}
 		
