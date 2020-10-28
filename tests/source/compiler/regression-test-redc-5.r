47a92f1911cf147bec3f06b100c650ee610b16ae
9214 <9214@protonmail.com>
2020-10-28 18:40:28 +0100

    TESTS: add regression test for issue #4569.

--- tests/source/compiler/regression-test-redc-5.r
+++ tests/source/compiler/regression-test-redc-5.r
@@ -254,6 +254,29 @@ test
 		--assert compiled?
 		--assert 3 = load qt/output
 	
+	--test-- "#4569"
+		--compile-and-run-this {
+			Red []
+
+			bind 'foo has [foo]['WTF]
+			foo: object []
+
+			probe foo
+			probe :foo
+		}
+		--assert compiled?
+		--assert [make object! [] make object! []] = load qt/output
+		
+		--compile-and-run-this {
+			Red []
+
+			block: reduce ['foo func [/bar]["Definitely not bar."]]
+			foo:  context [bar: does ['bar]]
+			print foo/bar
+		}
+		--assert compiled?
+		--assert 'bar = load qt/output
+		
 	--test-- "#4613"
 		--compile-this "Red [] probe bug$0"
 		--assert compilation-error?
