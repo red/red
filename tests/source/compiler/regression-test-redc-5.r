b63637551e34dd775cae06ee6f202795c4f622ae
hiiamboris <hiiamboris@gmail.com>
2022-02-15 20:37:07 +0300

    TESTS: for issue #5065

--- tests/source/compiler/regression-test-redc-5.r
+++ tests/source/compiler/regression-test-redc-5.r
@@ -315,6 +315,22 @@ test
 		--assert compiled?
 		--assert script-error?
 		
+	--test-- "#5065"
+		--compile-and-run-this {
+			Red []
+			do [
+				old: reduce list: [:loop :repeat :prin :exp :max :odd? :divide]
+				loop 1 [] repeat x 1 [] prin [] exp 1 max 1 0 odd? 2 divide 2 2 
+				new: reduce list
+				if all collect [
+					repeat i length? list [keep :old/:i =? :new/:i]
+				] [print "MATCH"]
+			]
+		}
+		--assert find qt/output "MATCH"
+	]
+]
+
 ===end-group===
 
 ~~~end-file~~~ 
