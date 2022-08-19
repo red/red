red/red

commit c7a4fb38837bcf14a0ea83f0c2b9d08cbae7871b

FIX: issue #5101 (Setting non-existing word in a context is silently accepted )

runtime/interpreter.reds
@@ -1161,7 +1161,7 @@ interpreter: context [
 				stack/mark-interp-native words/_set-path
 				pc: eval-expression pc end code no yes no	;-- yes: push value on top of stack
 				if tracing? [fire-event EVT_SET code pc value stack/get-top]
-				pc: eval-path value pc end code yes yes sub? no
+				pc: eval-path value pc end code yes no sub? no
 				either sub? [stack/unwind][stack/unwind-last]
 				if tracing? [value: stack/arguments]
 			]

commit 19a4f16ef26ed7fe5b669126f5fe967db80297f9

FIX: regression on setting object paths.
Caused by commit: https://github.com/red/red/commit/c7a4fb38837bcf14a0ea83f0c2b9d08cbae7871b

Relevant example:

    >> a: context [x: none]
    >> a/x: func [x][]
    *** Script Error: a/x: is missing its x argument
    *** Where: x
    *** Near : []
*** Stack: x

runtime/interpreter.reds
@@ -947,7 +947,7 @@ interpreter: context [
 			arg: either all [set? tail?][stack/arguments][null]
 			parent: actions/eval-path parent value arg path case? get? tail?
 			
-			unless get? [
+			if all [not get? not set?][
 				switch TYPE_OF(parent) [
 					TYPE_ACTION							;@@ replace with TYPE_ANY_FUNCTION
 					TYPE_NATIVE