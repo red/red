red/red

commit 4c14867e0da5f78ae27aca73a130a89567214a81

FIX: issue #5105 ([Windows] Click on a transparent box produces two suspicious on-over events)

modules/view/backends/windows/events.reds
@@ -98,6 +98,7 @@ get-event-offset: func [
 		value  [integer!]
 		msg    [tagMSG]
 		pt	   [tagPOINT]
+		pt-val [tagPOINT value]
 		gi	   [GESTUREINFO]
 		x	   [integer!]
 		y	   [integer!]
@@ -132,18 +133,13 @@ get-event-offset: func [
 			offset: as red-pair! stack/push*
 			offset/header: TYPE_PAIR
 			value: msg/lParam
-			
+
 			either evt/flags and EVT_FLAG_AWAY <> 0 [
-				msg: as tagMSG evt/msg
-				pt: declare tagPOINT
-				pt/x: 0 pt/y: 0
-				GetCursorPos pt
-				x: pt/x
-				y: pt/y
-				pt/x: 0 pt/y: 0
-				ClientToScreen msg/hWnd pt
-				offset/x: x - pt/x * 100 / dpi-factor
-				offset/y: y - pt/y * 100 / dpi-factor
+				pt-val/x: 0
+				pt-val/y: 0
+				ClientToScreen msg/hWnd :pt-val
+				offset/x: msg/x - pt-val/x * 100 / dpi-factor
+				offset/y: msg/y - pt-val/y * 100 / dpi-factor
 			][
 				offset/x: WIN32_LOWORD(value) * 100 / dpi-factor
 				offset/y: WIN32_HIWORD(value) * 100 / dpi-factor
@@ -1609,6 +1605,7 @@ process: func [
 		word   [red-word!]
 ][
 	flags: decode-down-flags msg/wParam
+	hWnd: msg/hWnd
 	switch msg/msg [
 		WM_MOUSEMOVE [
 			lParam: msg/lParam
@@ -1625,8 +1622,9 @@ process: func [
 			][
 				return EVT_DISPATCH						;-- filter out buggy mouse positions (thanks MS!)
 			]
-			saved: msg/hWnd
-			new: get-child-from-xy msg/hWnd x y
+			saved: hWnd
+			new: WindowFromPoint msg/x msg/y
+
 			if all [
 				IsWindowEnabled new
 				any [
@@ -1651,7 +1649,7 @@ process: func [
 		WM_MOUSELEAVE [
 			last-mouse-pt: -1
 			make-event msg EVT_FLAG_AWAY or key-flags EVT_OVER
-			if msg/hWnd = hover-saved [hover-saved: null]
+			if hWnd = hover-saved [hover-saved: null]
 			EVT_DISPATCH
 		]
 		WM_MOUSEWHEEL [
@@ -1663,15 +1661,15 @@ process: func [
 		WM_LBUTTONDOWN	[
 			menu-origin: null							;-- reset if user clicks on menu bar
 			menu-ctx: null
-			base-down-hwnd: msg/hWnd
+			base-down-hwnd: hWnd
 			res: make-event msg flags EVT_LEFT_DOWN
 			base-down-hwnd: null
 			res
 		]
 		WM_LBUTTONUP	[
-			prev-captured: msg/hWnd
-			if all [msg/hWnd <> null msg/hWnd = GetCapture not no-face? msg/hWnd][
-				word: (as red-word! get-face-values msg/hWnd) + FACE_OBJ_TYPE
+			prev-captured: hWnd
+			if all [hWnd <> null hWnd = GetCapture not no-face? hWnd][
+				word: (as red-word! get-face-values hWnd) + FACE_OBJ_TYPE
 				if base = symbol/resolve word/symbol [ReleaseCapture]	;-- issue #4384
 			]
 			res: make-event msg flags EVT_LEFT_UP
@@ -1686,7 +1684,7 @@ process: func [
 			pt: declare tagPOINT
 			pt/x: menu-x
 			pt/y: menu-y
-			ClientToScreen msg/hWnd pt
+			ClientToScreen hWnd pt
 			menu-origin: null
 			menu-ctx: null
 			res: make-event msg flags EVT_RIGHT_DOWN

commit 3d300ec6b9a00ecf4e59da87c8c97a23de491469

FIX: dragging face regression caused by commit 4c14867e0da5f78ae27aca73a130a89567214a81

modules/view/backends/windows/events.reds
@@ -1598,7 +1598,6 @@ process: func [
 		pt	   [tagPOINT]
 		hWnd   [handle!]
 		new	   [handle!]
-		saved  [handle!]
 		res	   [integer!]
 		x	   [integer!]
 		y	   [integer!]
@@ -1624,9 +1623,8 @@ process: func [
 			][
 				return EVT_DISPATCH						;-- filter out buggy mouse positions (thanks MS!)
 			]
-			saved: hWnd
-			new: WindowFromPoint msg/x msg/y
 
+			new: hWnd
 			if all [
 				IsWindowEnabled new
 				any [
@@ -1639,13 +1637,11 @@ process: func [
 					track/dwFlags: 2					;-- TME_LEAVE
 					track/hwndTrack: new
 					TrackMouseEvent :track
-					msg/hWnd: new
 				]
 				make-event msg flags EVT_OVER
 				key-flags: flags
 			]
 			hover-saved: new
-			msg/hWnd: saved
 			EVT_DISPATCH
 		]
 		WM_MOUSELEAVE [