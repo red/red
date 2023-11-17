red/red

commit 49d484d44425a426464d3d28b9f58d3427e0fd41

FIX: issue #5421 (Selecting an out-of-range index in a list widget causes an internal error)

modules/view/view.red
@@ -510,7 +510,8 @@ face!: object [				;-- keep in sync with facet! enum
 				selected
 				block? data
 				find [drop-list drop-down text-list field area] type
-				set-quiet 'text copy pick data selected 
+				data: pick data selected
+				set-quiet 'text copy data
 			]
 
 			system/reactivity/check/only self any [saved word]

commit f57cc85acc52a5dcc37f850261f10c55d7976b1b

FIX: regression in View caused by commit 49d484d
To reproduce the issue:
    view [field data now/date]

The field is not populated with the date value.

modules/view/view.red
@@ -510,8 +510,8 @@ face!: object [				;-- keep in sync with facet! enum
 				selected
 				block? data
 				find [drop-list drop-down text-list field area] type
-				data: pick data selected
-				set-quiet 'text copy data
+				value: pick data selected
+				set-quiet 'text copy value
 			]
 
 			system/reactivity/check/only self any [saved word]